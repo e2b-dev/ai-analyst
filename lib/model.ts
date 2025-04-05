f75f395eb45d5413a7d0e46b74aa876ac9561d59
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2025-04-06 00:26:56 +0100

    Added Llama 4 on Fireworks  (#10)
    * added llama4 (fireworks)
    
    * removed f1 models
    
    * made llama4 default

--- lib/model.ts
+++ lib/model.ts
@@ -1,4 +1,6 @@
 import { createOpenAI } from "@ai-sdk/openai";
+import { createFireworks } from "@ai-sdk/fireworks";
+import { createTogetherAI } from "@ai-sdk/togetherai";
 import { createOllama } from "ollama-ai-provider";
 
 export type LLMModel = {
@@ -27,13 +29,13 @@ export function getModelClient(model: LLMModel, config: LLMModelConfig) {
   const providerConfigs = {
     openai: () => createOpenAI()(modelNameString),
     togetherai: () =>
-      createOpenAI({
+      createTogetherAI({
         apiKey: apiKey || process.env.TOGETHER_API_KEY,
         baseURL: baseURL || "https://api.together.xyz/v1",
       })(modelNameString),
     ollama: () => createOllama({ baseURL })(modelNameString),
     fireworks: () =>
-      createOpenAI({
+      createFireworks({
         apiKey: apiKey || process.env.FIREWORKS_API_KEY,
         baseURL: baseURL || "https://api.fireworks.ai/inference/v1",
       })(modelNameString),
